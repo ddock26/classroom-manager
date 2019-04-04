39344a175531cb78070fff76f069903fd184eac4
manavm1990 <manavm@visionwebsoft.com>
2019-04-04 13:29:12 -0500

    Add theme.
    This includes a TODO.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,5 +3,13 @@ import Vuetify from "vuetify/lib";
 import "vuetify/src/stylus/app.styl";
 
 Vue.use(Vuetify, {
-  iconfont: "md"
+  iconfont: "md",
+  
+  // TODO: Update color theme.
+  theme: {
+    primary: "#3f51b5",
+    secondary: "#b0bec5",
+    accent: "#8c9eff",
+    error: "#b71c1c"
+  }
 });
