06e7e219bcf62d2d3ddc3d8343604074141090f8
manavm1990 <manavm@visionwebsoft.com>
2019-04-04 12:55:27 -0500

    Update comment about history mode.

--- src/router.js
+++ src/router.js
@@ -4,7 +4,14 @@ import Router from "vue-router";
 Vue.use(Router);
 
 export default new Router({
-  // https://router.vuejs.org/guide/essentials/history-mode.html#example-server-configurations
+  /**
+   * Using 'history' mode prevents the use of '#'.
+   * It allows server to not do anything - just serve up the index page,
+   * so Vue can take care of the routing.
+   * '#' is necessary for preventing a request to a server when 'linking.'
+   * This is akin to when we use 'anchor tag' (e.g. '#id')
+   * https://youtu.be/js5Sr_ZSHDs
+   * */
   mode: "history",
 
   base: process.env.BASE_URL,
