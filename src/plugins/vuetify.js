adbe5b97b1d97d05f784e463ae0f738958b7b9ee
manavm1990 <manavm@visionwebsoft.com>
2019-04-04 13:23:56 -0500

    Add theme.
    This includes TODO {alexander.rice}.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,5 +3,11 @@ import Vuetify from "vuetify/lib";
 import "vuetify/src/stylus/app.styl";
 
 Vue.use(Vuetify, {
-  iconfont: "md"
+  // TODO {alexander.rice}: Update color theme.
+  theme: {
+    primary: "#3f51b5",
+    secondary: "#b0bec5",
+    accent: "#8c9eff",
+    error: "#b71c1c"
+  }
 });
